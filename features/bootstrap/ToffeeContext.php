8dd0effe6daf31760069b2f6318d16b0f32a0196
Asheesh Rana <Head.Honcho@live.com>
2018-02-01 22:51:39 -0800

    Added behat.ym and renamed default featurecontext

--- features/bootstrap/ToffeeContext.php
+++ features/bootstrap/ToffeeContext.php
@@ -1,13 +1,11 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class ToffeeContext implements Context
 {
     private $toffeesEaten;
     private $toffeesReceived;
